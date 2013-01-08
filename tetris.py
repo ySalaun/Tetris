f88c754bca5f5a88e9cee894b9e0bdd85e4b29ff
Yohann <yohann.salaun@polytechnique.org>
2013-01-08 11:49:45 +0100

    added tasks + scene display

--- tetris.py
+++ tetris.py
@@ -14,6 +14,10 @@ Dialog = QtGui.QDialog()
 ui = tetris_playscreen.Ui_Dialog()
 ui.setupUi(Dialog)
 Dialog.show()
+#ui.screenP1.drawForeground(QPainter.drawLine(5, 8, 15, 35))
+scene = QGraphicsScene(0,0, int(ui.screenP1.width())-2,int(ui.screenP1.height())-2)
+ui.screenP1.setScene(scene)
+scene.addLine(2*int(ui.screenP1.width()),2*int(ui.screenP1.height()) , 0, 0)
 sys.exit(app.exec_())
  
 	
